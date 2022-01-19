b10bc2c0e409a69d6d8d6a0919ee259af3828927
Arnulfo Mendieta <mendieta.arnulfo@gmail.com>
2022-01-18 20:50:58 -0500

    Agregando alt a logo del sitio

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
     <div class="contenedor">
         <div class="barra-navegacion">
             <div class="logo">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo Sitio">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Gym Fitness">
             </div>
 
             <?php 
