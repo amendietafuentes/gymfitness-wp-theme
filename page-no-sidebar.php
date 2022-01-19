aeab69a0f0724f9827a71f711e7f434dc82ff14e
Arnulfo Mendieta <mendieta.arnulfo@gmail.com>
2022-01-18 19:40:50 -0500

    Agregando template parts a tema gymfitness

--- page-no-sidebar.php
+++ page-no-sidebar.php
@@ -9,19 +9,7 @@ get_header(); ?>
 
 <main class="contenedor pagina seccion no-sidebar">
     <div class="contenido-principal">
-        <?php while( have_posts() ): the_post();?>
-
-            <h1 class="text-center texto-primario"><?php the_title(); ?></h1>
-
-            <?php 
-                if( has_post_thumbnail() ): 
-                    the_post_thumbnail('blog', array('class'=>'imagen-destacada'));
-                endif;
-            ?>
-
-            <p><?php the_content(); ?></p>
-
-        <?php endwhile;?>
+        <?php get_template_part( 'template-parts/loop-pages')?>
     </div>
 
 </main>
--- page.php
+++ page.php
@@ -2,19 +2,7 @@
 
 <main class="contenedor pagina seccion con-sidebar">
     <div class="contenido-principal">
-        <?php while( have_posts() ): the_post();?>
-
-            <h1 class="text-center texto-primario"><?php the_title(); ?></h1>
-
-            <?php 
-                if( has_post_thumbnail() ): 
-                    the_post_thumbnail('blog', array('class'=>'imagen-destacada'));
-                endif;
-            ?>
-
-            <p><?php the_content(); ?></p>
-
-        <?php endwhile;?>
+        <?php get_template_part( 'template-parts/loop-pages')?>
     </div>
     
     <?php get_sidebar();?>
