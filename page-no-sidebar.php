e8434bdadba4bc600f6bfd05087af8319b590913
Arnulfo Mendieta <mendieta.arnulfo@gmail.com>
2022-01-18 19:21:16 -0500

    AgCreando theme para página de contacto y agregando estilos para tema sin sidebar

---
+++ page-no-sidebar.php
@@ -0,0 +1,29 @@
+
+<?php 
+
+/*
+* Template Name: Contenido Centrado (No Sidebars)
+*/
+
+get_header(); ?>
+
+<main class="contenedor pagina seccion no-sidebar">
+    <div class="contenido-principal">
+        <?php while( have_posts() ): the_post();?>
+
+            <h1 class="text-center texto-primario"><?php the_title(); ?></h1>
+
+            <?php 
+                if( has_post_thumbnail() ): 
+                    the_post_thumbnail('blog', array('class'=>'imagen-destacada'));
+                endif;
+            ?>
+
+            <p><?php the_content(); ?></p>
+
+        <?php endwhile;?>
+    </div>
+
+</main>
+
+<?php get_footer(); ?>
